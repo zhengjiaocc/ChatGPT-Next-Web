d23b38595ccaa7c589776ec4cd39dd938b9c72d8
zhengjiao <mailzhengjiao@163.com>
2026-04-21 12:59:13 +0800

    chore: 彻底删除 syncStore 中的全部旧云盘配置及冗余的 utils/cloud 工具协议，只保留了纯本地数据打包导出能力

--- app/store/sync.ts
+++ app/store/sync.ts
@@ -1,58 +1,25 @@
 import { getClientConfig } from "../config/client";
-import { ApiPath, STORAGE_KEY, StoreKey } from "../constant";
+import { StoreKey } from "../constant";
 import { createPersistStore } from "../utils/store";
 import {
   AppState,
   getLocalAppState,
-  GetStoreState,
   mergeAppState,
   setLocalAppState,
 } from "../utils/sync";
 import { downloadAs, readFromFile } from "../utils";
 import { showToast } from "../components/ui-lib";
 import Locale from "../locales";
-import { createSyncClient, ProviderType } from "../utils/cloud";
-
-export interface WebDavConfig {
-  server: string;
-  username: string;
-  password: string;
-}
 
 const isApp = !!getClientConfig()?.isApp;
-export type SyncStore = GetStoreState<typeof useSyncStore>;
-
-const DEFAULT_SYNC_STATE = {
-  provider: ProviderType.WebDAV,
-  useProxy: true,
-  proxyUrl: ApiPath.Cors as string,
-
-  webdav: {
-    endpoint: "",
-    username: "",
-    password: "",
-  },
-
-  upstash: {
-    endpoint: "",
-    username: STORAGE_KEY,
-    apiKey: "",
-  },
-
-  lastSyncTime: 0,
-  lastProvider: "",
-};
 
 export const useSyncStore = createPersistStore(
-  DEFAULT_SYNC_STATE,
+  {
+    lastSyncTime: 0,
+  },
   (set, get) => ({
-    cloudSync() {
-      const config = get()[get().provider];
-      return Object.values(config).every((c) => c.toString().length > 0);
-    },
-
     markSyncTime() {
-      set({ lastSyncTime: Date.now(), lastProvider: get().provider });
+      set({ lastSyncTime: Date.now() });
     },
 
     export() {
@@ -81,70 +48,9 @@ export const useSyncStore = createPersistStore(
         showToast(Locale.Settings.Sync.ImportFailed);
       }
     },
-
-    getClient() {
-      const provider = get().provider;
-      const client = createSyncClient(provider, get());
-      return client;
-    },
-
-    async sync() {
-      const localState = getLocalAppState();
-      const provider = get().provider;
-      const config = get()[provider];
-      const client = this.getClient();
-
-      try {
-        const remoteState = await client.get(config.username);
-        if (!remoteState || remoteState === "") {
-          await client.set(config.username, JSON.stringify(localState));
-          console.log(
-            "[Sync] Remote state is empty, using local state instead.",
-          );
-          return;
-        } else {
-          const parsedRemoteState = JSON.parse(
-            await client.get(config.username),
-          ) as AppState;
-          mergeAppState(localState, parsedRemoteState);
-          setLocalAppState(localState);
-        }
-      } catch (e) {
-        console.log("[Sync] failed to get remote state", e);
-        throw e;
-      }
-
-      await client.set(config.username, JSON.stringify(localState));
-
-      this.markSyncTime();
-    },
-
-    async check() {
-      const client = this.getClient();
-      return await client.check();
-    },
   }),
   {
     name: StoreKey.Sync,
-    version: 1.2,
-
-    migrate(persistedState, version) {
-      const newState = persistedState as typeof DEFAULT_SYNC_STATE;
-
-      if (version < 1.1) {
-        newState.upstash.username = STORAGE_KEY;
-      }
-
-      if (version < 1.2) {
-        if (
-          (persistedState as typeof DEFAULT_SYNC_STATE).proxyUrl ===
-          "/api/cors/"
-        ) {
-          newState.proxyUrl = "";
-        }
-      }
-
-      return newState as any;
-    },
+    version: 1.0,
   },
 );
